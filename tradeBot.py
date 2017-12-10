08eb8314b186c1ce14e5f50a0253b4c02fd456e4
Jeffrey <zic.jeffrey@gmail.com>
2017-12-10 12:35:15 -0500

    feat: Create access to record file
    Created access to record of trades in Google Sheets

--- tradeBot.py
+++ tradeBot.py
@@ -1,8 +1,80 @@
+from __future__ import print_function
+import httplib2
+import os
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
 import praw
 
-reddit = praw.Reddit('Trade Bot')
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
 
-subreddit = reddit.subreddit("orangeandblueleauge")
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/sheets.googleapis.com-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+CLIENT_SECRET_FILE = 'GoogleSheetsTradeSecret.json'
+APPLICATION_NAME = 'Trade Bot'
 
-trades = reddit.submission(id='6y39j2')
-print(trades)
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'sheets.googleapis.com-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def main():
+
+    reddit = praw.Reddit('Trade Bot')
+
+    subreddit = reddit.subreddit("orangeandblueleauge")
+
+    trades = reddit.submission(id='6y39j2') # Trade thread
+    trades.comments.replace_more(limit=None,threshold=0)
+    all_comments = trades.comments
+
+    print(all_comments[0].body)
+
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
+                    'version=v4')
+    service = discovery.build('sheets', 'v4', http=http,
+                              discoveryServiceUrl=discoveryUrl)
+
+    spreadsheetId = '1W7oJecLXON_C-cpCtPNe9sKH5gi55aq3jRQqWqH9jN4'
+    rangeName = 'Class Data!A2:E'
+    result = service.spreadsheets().values().get(
+        spreadsheetId=spreadsheetId, range=rangeName).execute()
+    numRows = result.get('values') if result.get('values')is not None else 0
+    print('{0} rows retrieved.'.format(numRows));
+
+if __name__ == '__main__':
+    main()
+
