a906440b9726cc9b15f4af1c0a680cc70b2e0500
Jeffrey <zic.jeffrey@gmail.com>
2017-12-10 11:05:32 -0500

    feat: grabs trade post
    Now grabs and stores the data from the appropriate trade post

--- tradeBot.py
+++ tradeBot.py
@@ -1,5 +1,8 @@
 import praw
 
-reddit = praw.Reddit('TradeBot')
+reddit = praw.Reddit('Trade Bot')
 
 subreddit = reddit.subreddit("orangeandblueleauge")
+
+trades = reddit.submission(id='6y39j2')
+print(trades)
